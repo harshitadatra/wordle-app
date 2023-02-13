3897fe3dbaa7d8a6a21aefdf585487df4a97ff94
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2023-02-14 02:27:29 +0530

    feat: json server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { useEffect, useState } from "react";
 
 function App() {
+  const [solution,setSolution] = useState("")
+  useEffect(()=>{
+    fetch('http://localhost:3001/solutions').then( res => res.json()).then(json => console.log(json))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
