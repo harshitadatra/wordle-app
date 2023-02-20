04cdb77b961df566a2933f7f96a529a037518eb4
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2023-02-21 01:46:27 +0530

    feat: completed tracking the current guess

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
+import "./index.css"
 
 function App() {
-  const [solution,setSolution] = useState("")
-  useEffect(()=>{
-    fetch('http://localhost:3001/solutions').then( res => res.json()).then(json => console.log(json))
-  },[])
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        // random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>wordle </h1>
-      </header>
-    </div>
+      <h1>Wordle (Lingo)</h1>
+      {solution && <Wordle solution={solution} />}
+      </div>
   );
 }
 
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 const useWordle = (solution) => 
 {
     const [turn,setTurn] = useState(0);
-    const [currentGuess,setCurrentGuess] = useState(0);
-    const [guesses,setGuesses] = useState([]);
-    const [history,setHistory] = useState([]);
+    const [currentGuess,setCurrentGuess] = useState("");
+    const [guesses,setGuesses] = useState([]); // each guess is an array
+    const [history,setHistory] = useState([]); // each guess is a string
     const [isCorrect,setIsCorrect] = useState(false);
     const formatGuess =() =>
     {
@@ -14,9 +14,19 @@ const useWordle = (solution) =>
     {
 
     }
-    const handleKeyup= () =>
+    const handleKeyup= ({key}) =>
     {
+          console.log("key pressed - ", key);
 
+          if (key === "Backspace") {
+            setCurrentGuess((prev) => prev.slice(0, -1));
+            return;
+          }
+          if (/^[A-Za-z]$/.test(key)) {
+            if (currentGuess.length < 5) {
+              setCurrentGuess((prev) => prev + key);
+            }
+          }
     }
     return {turn,currentGuess,guesses,history,isCorrect,handleKeyup}
 }
